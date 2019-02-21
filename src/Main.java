ace34d767dcca6ed6d52193b8f81d015964d892e
HusakVol <vol.husak@gmail.com>
2019-02-21 17:11:54 +0200

    Created 'MyCollection' for fast inserting elements to the middle of collection

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,29 @@
-import mycollections.MyList;
+import mycollections.MyCollection;
+
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 
+        LinkedList<Integer> list = new LinkedList<>();
+
+        int n = 1000000;
+
+        MyCollection<Integer> collection = new MyCollection<>();
+        long timeForMyCollection = System.nanoTime() * -1;
+        for (int i = 0; i < n; i++) {
+            collection.addToMiddle(i);
+        }
+        timeForMyCollection += System.nanoTime();
+
+        System.out.print("\nMyCollection:\t" + timeForMyCollection);
+
+
+        long timeForLinkedList = System.nanoTime() * -1;
+        for(int i = 0; i < n; i++)
+            list.add(list.size()/2, i);
+        timeForLinkedList -= System.nanoTime();
+        System.out.println("LinkedList:\t" + timeForLinkedList);
     }
 }
---
+++ src/mycollections/MyCollection.java
@@ -0,0 +1,125 @@
+package mycollections;
+
+import java.lang.reflect.Executable;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class MyCollection<E> implements Collection {
+    public MyCollection() {
+
+    }
+
+    LinkedList<E> firstSegment;
+    LinkedList<E> secondSegment;
+
+    public boolean addToMiddle(Object o) {
+        try {
+            if (firstSegment.size() > secondSegment.size())
+                secondSegment.addFirst((E) o);
+            else
+                firstSegment.addLast((E) o);
+        } catch (Exception e) {
+            return false;
+        }
+        balance();
+        return true;
+    }
+
+    @Override
+    public int size() {
+        return firstSegment.size() + secondSegment.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return firstSegment.size() + secondSegment.size() == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return firstSegment.contains(o) || secondSegment.contains(o);
+    }
+
+    @Override
+    public Iterator iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return null;
+    }
+
+    private void balance() {
+        int difference = Math.abs(firstSegment.size() - secondSegment.size());
+        if (difference < 2)
+            return;
+        if (firstSegment.size() > secondSegment.size()) {
+            for (int i = 0; i < difference / 2; i++) {
+                secondSegment.addFirst(firstSegment.pollLast());
+            }
+            return;
+        }
+        for (int i = 0; i < difference / 2; i++) {
+            firstSegment.addLast(secondSegment.pollFirst());
+        }
+    }
+
+    @Override
+    public boolean add(Object o) {
+        try {
+            secondSegment.add((E) o);
+        } catch (Exception e) {
+            return false;
+        }
+        balance();
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        boolean result =  firstSegment.remove(o) || secondSegment.remove(o);
+        balance();
+        return result;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        boolean result = secondSegment.addAll(c);
+        balance();
+        return result;
+    }
+
+    @Override
+    public void clear() {
+        firstSegment.clear();
+        secondSegment.clear();
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+//        LinkedList<E> allList;
+        throw new UnsupportedOperationException();
+//        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        boolean result =  firstSegment.remove(c) || secondSegment.remove(c);
+        balance();
+        return result;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
+
+}
